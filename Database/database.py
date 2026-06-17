a9e7b7649339d1c41cbd5c8b566efbfb110383ea
Shashi Kumar <shashiranjan.kr92@gmail.com>
2026-06-17 19:24:52 +0530

    update env variable

--- Database/database.py
+++ Database/database.py
@@ -2,10 +2,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+import os
 
 
 ##DATABASE_URL = "sqlite:///inventory.db"
-DATABASE_URL = "postgresql+psycopg2://localhost:5433/inventory_db"
+#DATABASE_URL = "postgresql+psycopg2://localhost:5433/inventory_db"
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:your_password@localhost:5433/inventory_db"
+)
+
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL environment variable is not set")
 
 engine = create_engine(DATABASE_URL)
 
